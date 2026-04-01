907ee451c6cd319c7129c37ece97026f21feba2b
Gargleblaster-RMBK <gargleblaster.rmbk@gmail.com>
2026-04-01 10:28:43 -0500

    Added component test page, please do not include in prod.
    Signed-off-by: Gargleblaster-RMBK <gargleblaster.rmbk@gmail.com>

---
+++ src/routes/dev/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+	import Header from '$lib/components/Header.svelte'
+	import Footer from '$lib/components/Footer.svelte'
+	import CHITEM from '$lib/components/ChecklistItem.svelte'
+	import CRIND from '$lib/components/ControlRoomIndicator.svelte'
+	import '../../app.css'
+</script>
+<div class="p-16 gap-50">
+	<!-- With Button -->
+	<Header
+		heroImg="/images/capybara.png"
+		title="Example Title"
+		catchphrase="Example Catchphrase"
+		button="https://example.org"
+		buttontext="Example"
+	/>
+	<!-- Without Button-->
+	<Header
+		heroImg="/images/capybara.png"
+		title="Example Title"
+		catchphrase="Example Catchphrase"
+	/>
+</div>
+
+<div class="p-xl gap-50">
+	<Footer />
+</div>
+
+<div class="p-xl gap-50">
+	<CHITEM item="Example Item" action="Example Action" actionColor="1" indent />
+	<CHITEM item="Example Item" action="Example Action" actionColor="2" indent />
+	<CHITEM item="Example Item" action="Example Action" actionColor="3" indent />
+	<CHITEM item="Example Item" action="Example Action" actionColor="4" indent />
+	<CHITEM item="Example Item" action="Example Action" actionColor="1" />
+	<CHITEM item="Example Item" action="Example Action" actionColor="2" />
+	<CHITEM item="Example Item" action="Example Action" actionColor="3" />
+	<CHITEM item="Example Item" action="Example Action" actionColor="4" />
+</div>
+
+<div class="p-xl gap-50">
+	<CRIND name="Example Name" description="Example Name" color="white" />
+	<CRIND name="Example Name" description="Example Name" color="yellow" />
+	<CRIND name="Example Name" description="Example Name" color="red" />
+</div>
